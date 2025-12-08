b3927cf18a876e39625ab4b36bc96b062d2d1339
shadmanadman <adman.shadman@gmail.com>
2025-12-08 14:55:10 +0330

    init

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidKotlinMultiplatformLibrary)
+}
+
+kotlin {
+    // Android library
+    androidLibrary {
+        namespace = "wolt.kmp.lib"
+        compileSdk = 36
+        minSdk = 36
+    }
+
+    // iOS Framework
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "woltKit"
+            isStatic = true
+        }
+    }
+
+    sourceSets{
+        commonMain.dependencies {  }
+        androidMain.dependencies {  }
+        iosMain.dependencies {  }
+    }
+}
