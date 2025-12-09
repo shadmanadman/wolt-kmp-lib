e85528d82bf826b0e78b7db9605d06fa34ed9288
shadmanadman <adman.shadman@gmail.com>
2025-12-09 22:42:07 +0330

    fix using the old android library plugin

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidKotlinMultiplatformLibrary)
@@ -9,10 +6,6 @@ plugins {
 kotlin {
     // Android library
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
         publishLibraryVariants("release")
     }
 
